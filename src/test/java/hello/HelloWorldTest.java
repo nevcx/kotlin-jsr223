14324ecf2205c2c2f60cd98e43d85a07e1ad270d
Mats Karlsson <matsk@wip.se@>
2020-05-18 11:47:57 +0200

    Renamed test function and added test for javascript also

--- src/test/java/hello/HelloWorldTest.java
+++ src/test/java/hello/HelloWorldTest.java
@@ -14,12 +14,27 @@ public class HelloWorldTest {
     }
  
     @Test
-    public void aFastTest() {
+    public void kotlinTest() {
         try
         {
             String scriptcode = "\n\nthrow Exception()";
             ScriptEngineManager factory = new ScriptEngineManager();
             ScriptEngine engine = factory.getEngineByExtension("kts");
+            engine.eval(scriptcode);
+        }
+        catch(ScriptException se)
+        {
+            assertEquals(3, se.getLineNumber());
+        }
+    }
+
+    @Test
+    public void javascriptTest() {
+        try
+        {
+            String scriptcode = "\n\nthrow 'Exception'";
+            ScriptEngineManager factory = new ScriptEngineManager();
+            ScriptEngine engine = factory.getEngineByExtension("js");
             engine.eval(scriptcode);
         }
         catch(ScriptException se)
